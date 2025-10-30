654d5f49657334d75a7ed26face329d27011507e
Vanya Tochevska <vtochevska@gmail.com>
2025-10-30 09:36:09 +0000

    updated token expiry time

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -11,7 +11,7 @@ class Settings(BaseSettings):
     # Security
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 240  # 4 hours
 
     # CORS
     BACKEND_CORS_ORIGINS: List[str] = ["http://localhost:4200"]
